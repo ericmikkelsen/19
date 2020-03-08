45bff744e80ed4172ef982b55a9cb450a15e3998
eric mikkelsen <eric@ericmikkelsen.com>
2020-03-07 19:23:40 -0800

    feat: adds 404 options for 11ty serve

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,23 @@
+const fs = require("fs");
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.setLiquidOptions({
-      dynamicPartials: true
-    });
-  };
-  
-//   module.exports = function(eleventyConfig) {
-//     eleventyConfig.addPassthroughCopy("assets/fonts");
-//     eleventyConfig.addPassthroughCopy("assets/css");
-//   };
+  eleventyConfig.addPassthroughCopy("_redirects");
+  eleventyConfig.addPassthroughCopy("assets/style.css");
+
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, bs) {
+        const content_404 = fs.readFileSync("_site/404.html");
+
+        bs.addMiddleware("*", (req, res) => {
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          // Add 404 http status code in request header.
+          // res.writeHead(404, { "Content-Type": "text/html" });
+          res.writeHead(404);
+          res.end();
+        });
+      }
+    }
+  });
+};
